a95dd3876509a196dd3fff5ff9c0aef86b829cb9
diegogarcia9907 <dfgarcia43@ucatolica.edu.co>
2024-02-10 12:31:04 -0300

    se sube proyecto

---
+++ OneDrive/Documentos/NetBeansProjects/lab_1/src/main/java/com/mycompany/lab_1/Ave.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab_1;
+
+/**
+ *
+ * @author diego
+ */
+public abstract class Ave {
+    
+   public abstract void makeSound(); 
+    
+}
---
+++ OneDrive/Documentos/NetBeansProjects/lab_1/src/main/java/com/mycompany/lab_1/Lab_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab_1;
+
+/**
+ *
+ * @author diego
+ */
+public class Lab_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
